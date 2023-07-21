2c75d8f2d1b982c4d88bd9b65b09922d7fef3d71
Heensle <hallie@stringham.name>
2023-07-21 14:21:17 -0400

    Adjusted controls

--- main.py
+++ main.py
@@ -67,35 +67,35 @@ def main():
         elif cursor_x > 240:
             doodler = doodler_orig
 
-        if cursor_x < 240 and cursor_x >= 200:
-            doodler_x -= 1
+        if cursor_x < 230 and cursor_x >= 200:
+            doodler_x -= 0.5
         elif cursor_x < 200 and cursor_x >= 150:
-            doodler_x -= 2
+            doodler_x -= 1
         elif cursor_x < 150 and cursor_x >= 90:
-            doodler_x -= 3
+            doodler_x -= 2
         elif cursor_x < 90 and cursor_x >= 30:
-            doodler_x -= 4
+            doodler_x -= 3.5
         elif cursor_x < 30 and cursor_x >= 0:
             doodler_x -= 5
         elif cursor_x < 0:
             doodler_x -= 10
-        elif cursor_x > 240 and cursor_x <= 280:
-            doodler_x += 1
+        elif cursor_x > 250 and cursor_x <= 280:
+            doodler_x += 0.5
         elif cursor_x > 280 and cursor_x <= 320:
-            doodler_x += 2
+            doodler_x += 1
         elif cursor_x > 320 and cursor_x <= 380:
-            doodler_x += 3
+            doodler_x += 2
         elif cursor_x > 380 and cursor_x <= 450:
-            doodler_x += 4
+            doodler_x += 3.5
         elif cursor_x > 450 and cursor_x <= 480:
             doodler_x += 5
         elif cursor_x > 480:
             doodler_x += 10
 
-        if doodler_x < 0:
+        if doodler_x < -60:
             doodler_x = 480
         elif doodler_x > 480:
-            doodler_x = 0
+            doodler_x = -60
 
         screen.blit(background, (0, 0))
         screen.blit(platform, (23, 572))
