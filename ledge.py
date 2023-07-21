4f9ad069130985736b7e17e24b13b01f9e67edb5
Heensle <hallie@stringham.name>
2023-07-21 14:58:22 -0400

    Added member function names

--- ledge.py
+++ ledge.py
@@ -6,14 +6,14 @@ class platform:
         self.x_pos = random_num.platform_rand_x()
         self.y_pos = random_num.platform_rand_y()
 
-    def __init__ (self, start_range, end_range):
+    def x_range (self, start_range, end_range):
         self.x_pos = random_num.platform_controlled_x(start_range, end_range)
         self.y_pos = random_num.platform_rand_y
 
-    def __init__ (self, prev_y):
+    def relational_y (self, prev_y):
         self.x_pos = random_num.platform_rand_x
         self.y_pos = random_num.platform_relational_y(prev_y)
 
-    def __init__ (self, start_range, end_range, prev_y):
+    def x_range_relational_y (self, start_range, end_range, prev_y):
         self.x_pos = random_num.platform_controlled_x(start_range, end_range)
         self.y_pos = random_num.platform_relational_y(prev_y)
