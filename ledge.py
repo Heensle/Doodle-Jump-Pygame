47b4c99291888e6c7a707ece7ad8a37d55452fed
Heensle <hallie@stringham.name>
2023-07-21 15:06:41 -0400

    Added image data member

--- ledge.py
+++ ledge.py
@@ -2,18 +2,22 @@ import random_num
 
 class platform:
     
-    def __init__ (self):
+    def __init__ (self, image):
         self.x_pos = random_num.platform_rand_x()
         self.y_pos = random_num.platform_rand_y()
+        self.image = image
 
-    def x_range (self, start_range, end_range):
+    def x_range (self, start_range, end_range, image):
         self.x_pos = random_num.platform_controlled_x(start_range, end_range)
         self.y_pos = random_num.platform_rand_y
+        self.image = image
 
-    def relational_y (self, prev_y):
+    def relational_y (self, prev_y, image):
         self.x_pos = random_num.platform_rand_x
         self.y_pos = random_num.platform_relational_y(prev_y)
+        self.image = image
 
-    def x_range_relational_y (self, start_range, end_range, prev_y):
+    def x_range_relational_y (self, start_range, end_range, prev_y, image):
         self.x_pos = random_num.platform_controlled_x(start_range, end_range)
         self.y_pos = random_num.platform_relational_y(prev_y)
+        self.image = image
