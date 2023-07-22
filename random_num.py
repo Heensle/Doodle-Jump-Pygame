e81d575895bf2b214d142bdd4fa199f15981e739
Heensle <hallie@stringham.name>
2023-07-22 16:40:45 -0400

    Updated class name change

--- random_num.py
+++ random_num.py
@@ -1,9 +1,9 @@
 import random
 
-def platform_rand_x():
+def ledge_rand_x():
     return random.randint(0, 389) #all possible pixel values in which the platform is fully on screen
 
-def platform_controlled_x(start_range, end_range): #takes start and end of disruptive object to prevent platform generation on top of the object
+def ledge_controlled_x(start_range, end_range): #takes start and end of disruptive object to prevent platform generation on top of the object
     option_one = random.randint(0, start_range - 91)
     option_two = random.randint(end_range, 389)
     if random.randint(0, 1):
@@ -11,8 +11,8 @@ def platform_controlled_x(start_range, end_range): #takes start and end of disru
     else:
         return option_two
     
-def platform_rand_y():
+def ledge_rand_y():
     return random.randint(0, 800)
 
-def platform_relational_y(prev_y):
-    return random.randint(prev_y, prev_y + 250)
+def ledge_relational_y(prev_y):
+    return random.randint((prev_y - 250), prev_y)
