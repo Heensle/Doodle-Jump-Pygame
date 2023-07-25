4d40d38741f04439f3e5ad11cf8d64934b0e76df
Heensle <hallie@stringham.name>
2023-07-24 21:49:45 -0400

    Created hitbox, formatted

--- Doodler.py
+++ Doodler.py
@@ -2,10 +2,11 @@ import pygame
 
 class Doodler(pygame.sprite.Sprite):
 
-    def __init__(self, image, x, y, speed):
+    def __init__(self, image, hitbox, x, y, speed):
         pygame.sprite.Sprite.__init__(self)
         self.image = image
-        self.rect = image.get_rect
+        self.hitbox = hitbox
+        self.rect = hitbox.get_rect()
         self.x_pos = x
         self.y_pos = y
         self.speed = speed
